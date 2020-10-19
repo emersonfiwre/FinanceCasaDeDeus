0bbd6f0ef360a51c96ee159be48aaee8b0fa509d
emersonfiwre <lima.emerson2009@gmail.com>
2020-10-19 10:50:46 -0300

    screen login started

---
+++ app/src/main/java/br/com/casadedeus/LoginActivity.kt
@@ -0,0 +1,16 @@
+package br.com.casadedeus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun onLogin(view: View){
+        println("click open MainActivity")
+    }
+}
