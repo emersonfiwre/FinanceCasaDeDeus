693a703254550ebdced36db96143c43f00b3ec62
Server-Jin.i5 <lima.emerson2009@gmail.com>
2020-10-30 17:02:01 -0300

    implements bottom sheet dialog, radio button and designers for add expenditure

--- app/src/main/java/br/com/casadedeus/fragment/MonthFragment.kt
+++ app/src/main/java/br/com/casadedeus/fragment/MonthFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,26 +13,31 @@ import androidx.recyclerview.widget.RecyclerView
 import br.com.casadedeus.R
 import br.com.casadedeus.adapter.ExpenditureAdapter
 import br.com.casadedeus.model.ExpenditureModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.fragment_month.*
 import kotlinx.android.synthetic.main.fragment_month.view.*
 
-class MonthFragment: Fragment() {
+class MonthFragment : Fragment() {
 
     companion object {
-        fun newInstance(month : String): MonthFragment {
+        fun newInstance(month: String): MonthFragment {
             val args = Bundle()
-            args.putString("month",month);
+            args.putString("month", month);
             val fragment = MonthFragment()
             fragment.arguments = args
             return fragment
         }
     }
-
+    /*Design
+    * radio button https://dribbble.com/shots/9890260-Card-Theme-Switch-Light-Theme
+    * btnSave top rigth https://stackoverflow.com/questions/58651661/how-to-set-max-height-in-bottomsheetdialogfragment*/
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_month,container,false)
+        val view = inflater.inflate(R.layout.fragment_month, container, false)
         val activity: Context = activity as Context
         val addLancamento = view.findViewById<Button>(R.id.add_lancamento)
         //****************** NÃO É NECESSÁRIO FINDVIEWBYID
@@ -43,17 +47,27 @@ class MonthFragment: Fragment() {
         //************
         val rvExpenditure = view.findViewById<RecyclerView>(R.id.rv_expenditure)
         val model = ExpenditureModel()
-        val adapter = ExpenditureAdapter(activity,model.getExpenditures())
+        val adapter = ExpenditureAdapter(activity, model.getExpenditures())
         rvExpenditure.adapter = adapter
         val linearLayoutManager = LinearLayoutManager(activity)
         rvExpenditure.layoutManager = linearLayoutManager
 
         view.back_month.setOnClickListener { getActivity()?.onBackPressed() }
 
-
         val month = arguments?.getString("month") as String
         view.month.text = month
 
+        view.add_lancamento.setOnClickListener {
+            val dialog = BottomSheetDialog(activity)
+            val bottomSheet = layoutInflater.inflate(R.layout.dialog_single_input, null)
+            dialog.setContentView(bottomSheet)
+            /*val mBehavior = BottomSheetBehavior.from(bottomSheet.parent as View);
+            mBehavior.setPeekHeight(600)*/
+            dialog.show()
+        }
+
         return view
     }
+
+
 }
