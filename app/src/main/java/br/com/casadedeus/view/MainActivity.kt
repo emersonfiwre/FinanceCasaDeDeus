910f4794c57c88cf59f7aae58c898250cefb00ce
emersonfiwre <lima.emerson2009@gmail.com>
2021-01-19 15:45:15 -0300

    Inflar o botão de adicionar o planejamento na toolbar, fazer o login

--- app/src/main/java/br/com/casadedeus/view/MainActivity.kt
+++ app/src/main/java/br/com/casadedeus/view/MainActivity.kt
@@ -1,9 +1,13 @@
 package br.com.casadedeus.view
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import br.com.casadedeus.R
-import br.com.casadedeus.service.constants.ViewConstants
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     //    private lateinit var mYearFragment: YearFragment
@@ -15,14 +19,22 @@ class MainActivity : AppCompatActivity() {
         //https://dribbble.com/shots/14295333-Online-banking-finance-app-concept
         //https://dribbble.com/shots/7407699--Sign-Up-Sign-In-Modal-Windows
         // Carrega dados do usuário, caso haja
+        /*
+        Antiga chamada do fragment
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.container_root, TransactionFragment(), ViewConstants.TAGS.TRANSACTION_FRAG)
                 .commit()
-        }
+        }*/
 
+        // Navegação
+        bottom_navigation.setupWithNavController(
+            Navigation.findNavController(
+                this,
+                R.id.nav_host_fragment
+            )
+        )
     }
-
-
+    
 }
---
+++ app/src/main/java/br/com/casadedeus/view/PlanningFragment.kt
@@ -0,0 +1,91 @@
+package br.com.casadedeus.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.com.casadedeus.R
+import br.com.casadedeus.beans.TransactionModel
+import br.com.casadedeus.service.constants.ViewConstants
+import br.com.casadedeus.view.adapter.TransactionAdapter
+import br.com.casadedeus.viewmodel.TransactionViewModel
+import kotlinx.android.synthetic.main.fragment_planning.view.*
+
+
+class PlanningFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var mViewRoot: View
+    private lateinit var mViewModel: TransactionViewModel
+    private val mAdapter: TransactionAdapter = TransactionAdapter()
+
+
+    /*Design
+    * radio button https://dribbble.com/shots/9890260-Card-Theme-Switch-Light-Theme
+    * btnSave top rigth https://stackoverflow.com/questions/58651661/how-to-set-max-height-in-bottomsheetdialogfragment
+    * ?android:textColorHint*/
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mViewRoot = inflater.inflate(R.layout.fragment_planning, container, false)
+        mViewModel = ViewModelProvider(this).get(TransactionViewModel::class.java)
+
+        //************
+        setupRecycler()
+
+        // Criar observadores
+        observer()
+
+        setListeners()
+
+        //for crate home button
+        val activity = activity as AppCompatActivity?
+        activity?.setSupportActionBar(mViewRoot.toolbar)
+        //activity?.supportActionBar?.title = "My Title"
+
+
+        return mViewRoot
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    private fun setupRecycler() {
+        val linearLayoutManager = LinearLayoutManager(context)
+        mViewRoot.rv_planning.layoutManager = linearLayoutManager
+        //mAdapter.attachListener()
+        mViewRoot.rv_planning.adapter = mAdapter
+    }
+
+    private fun setListeners() {
+
+    }
+
+    private fun observer() {
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+        }
+    }
+
+    private fun showPlanning(transaction: TransactionModel? = null) {
+        val intent = Intent(context, TransactionFormActivity::class.java)
+        val bundle = Bundle()
+        bundle.putSerializable(ViewConstants.KEYS.TRANSACTION, transaction)
+        intent.putExtras(bundle)
+        startActivity(intent)
+        activity?.overridePendingTransition(
+            R.anim.slide_in_up,
+            R.anim.slide_out_up
+        )
+    }
+
+
+}
